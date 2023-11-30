4596ae662e0f6553a5a15907ae07205451fb8e66
Minh <minhans29@gmail.com>
2023-11-30 22:38:42 +0700

    chapper1 lesson12

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ function App() {
     return `${firstName} ${lastName}`;
   }
 
+  function Feature(props) {
+    console.log(props);
+    return (
+    <div className="feature">
+      <img src={props.image} alt="natural picture" className="feature-image"></img>
+      <h3 className="feature-title">Title {props.title}</h3>
+      <p className="feature-desc">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+      </p>
+    </div>
+    );
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Feature image="https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg"
+      title="Honda"></Feature>
+      <Feature></Feature>
+      <Feature></Feature>
     </div>
   );
 }
--- src/jsx.js
+++ src/jsx.js
@@ -3,22 +3,23 @@ import React from "react";
 const jsx = () => {
   const element1 = <div id="root">Hello world</div>;
   const element2 = React.createElement("div", { id: "root" }, "Hello world");
-  return <div> </div>;
-};
+  const element3 = (
+    <div>
+      <span>Hello</span>
+      <span>World</span>
+    </div>
+  );
 
-const element3 = (
-  <div>
-    <span>Hello</span>
-    <span>World</span>
+  const element4 = React.createElement("div", {
+    children: [
+      React.createElement("span", null, "Hello"),
+      " ",
+      React.createElement("span", null, "World"),
+    ],
+  });
+  return <div className="heading" id="heading">
+    this is heading 
   </div>
-);
-
-const element4 = React.createElement("div", {
-  children: [
-    React.createElement("span", null, "Hello"),
-    " ",
-    React.createElement("span", null, "World"),
-  ],
-});
+};
 
 export default jsx;
