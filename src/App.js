345eb963d1b81603f5f9baa13d510a8ff089439c
Minh <minhans29@gmail.com>
2023-12-01 09:15:23 +0700

    chapper2 props
    install CRA
    unserstand jsx and how to use it
    components
    styling
    redering list
    props and props children

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { FeatureData } from "./FeatureData";
+
 
 function App() {
   const name = "Ahihi";
@@ -7,30 +9,55 @@ function App() {
     return `${firstName} ${lastName}`;
   }
 
+  // const FeatureData = [
+  //   {
+  //     image:
+  //       "https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg",
+  //     title: "Honda",
+  //   },
+  //   {
+  //     image:
+  //       "https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg",
+  //     title: "Mec",
+  //   },
+  //   {
+  //     image:
+  //       "https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg",
+  //     title: "Posche",
+  //   },
+  // ];
+
   function Feature(props) {
     console.log(props);
     return (
-    <div className="feature">
-      <img src={props.image} alt="natural picture" className="feature-image"></img>
-      <h3 className="feature-title">Title {props.title}</h3>
-      <p className="feature-desc">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-      </p>
-    </div>
+      <div className="feature">
+        <img
+          src={props.image}
+          alt="natural picture"
+          className="feature-image"
+        ></img>
+        <h3 className="feature-title">Title {props.title}</h3>
+        <p className="feature-desc">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum
+        </p>
+      </div>
     );
   }
 
-
   return (
-    <div>
-      <Feature image="https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg"
-      title="Honda"></Feature>
-      <Feature></Feature>
-      <Feature></Feature>
+    <div className="feature-list">
+      {FeatureData.map((item) => {
+        return <Feature image={item.image} title={item.title}></Feature>;
+      })}
+      ;
     </div>
   );
 }
---
+++ src/FeatureData.js
@@ -0,0 +1,20 @@
+export const FeatureData = () =>{
+    return (
+        [
+            {
+                "image": "https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg",
+                "title": "Honda"
+        
+            },
+            {
+                "image": "https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg",
+                "title": "Mec"
+            },
+            {
+                "image": "https://i.pinimg.com/550x/a7/87/20/a78720c39a39ac50a2856420d636d113.jpg",
+                "title": "Posche"
+            }
+        ]
+    )
+} 
+    
